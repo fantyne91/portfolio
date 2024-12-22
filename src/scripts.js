75870041857f5ef1a8e034c490afa6baff20f961
Maria <97641102+fantyne91@users.noreply.github.com>
2024-12-23 00:37:52 +0100

    landing ready+jspdf

---
+++ src/scripts.js
@@ -0,0 +1,70 @@
+
+
+
+import { Landing } from '../components/Landing.js';
+import { Uxui } from '../components/Uxui.js';
+//import { Programming } from './components/Programming.js';
+//import { Games } from './components/Games.js';
+//import { Unreal } from './components/Unreal.js';
+
+
+
+//Instanciar vue al hacer click o eventos
+const app = Vue.createApp({
+  //Datos que van a ser reactivos
+  data() {
+    return {
+      currentPage: 'Landing', // Nombre del componente que se mostrará
+      //currentPageClass: 'landing', // Clase que se aplica dinámicamente
+      menuOpen: false, // Estado del menú hamburguesa
+      isMobileOrTablet: false, // Indicador para tamaño de pantalla
+    }
+  },
+
+  // computed: {
+  //   // Computed property para generar la clase dinámica según la página actual
+  //   currentPageClassComputed() {
+  //     return `page-${this.currentPage.toLowerCase()}`
+  //   },
+  // },
+
+  //funciones que hacen reaccion
+  methods: {
+    loadPage(page) {
+      console.log('Cambiando a la página:', page) // Verifica que el valor sea correcto
+
+      this.currentPage = page // Cambia el valor de currentPage
+      this.menuOpen = false
+    },
+    toggleMenu() {
+      this.menuOpen = !this.menuOpen
+    },
+    updateScreenSize() {
+      this.isMobileOrTablet = window.innerWidth < 800 // Detecta si es menor a 768px
+      if (!this.isMobileOrTablet) {
+        this.menuOpen = false // Cierra el menú si cambia a escritorio
+      }
+    },
+  },
+  mounted() {
+    // Similar a "created" pero en createApp
+    this.updateScreenSize() // Comprobamos el tamaño al iniciar
+    window.addEventListener('resize', this.updateScreenSize) // Detectamos cambios
+  },
+  beforeUnmount() {
+    // Similar a "beforeDestroy"
+    window.removeEventListener('resize', this.updateScreenSize) // Limpiamos el evento
+  },
+})
+
+// Registrar los componentes
+app.component('Landing', Landing);
+app.component('Uxui', Uxui);
+//app.component('Programming', Programming);
+//app.component('Games', Games);
+//app.component('Unreal', Unreal);
+
+// Montar la aplicación Vue en el elemento con id="app"
+app.mount('#app');
+
+
