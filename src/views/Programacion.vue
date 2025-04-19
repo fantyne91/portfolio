e291a82868a699ad563d204df8b876bea628912f
Maria <97641102+fantyne91@users.noreply.github.com>
2025-04-19 11:56:47 +0200

    1.4.5 kofi

--- src/views/Programacion.vue
+++ src/views/Programacion.vue
@@ -267,7 +267,7 @@ onMounted(() => {
             <img src="/images/busqueda.webp" loading="lazy" alt="imagen app web animal" width="300">
             <img src="/images/landing-animal.webp" loading="lazy" alt="imagen app landing web animal" width="300">
           </div>
-          <div class="donacion-plataforma ">
+          <div id="donacion-plataforma" class="donacion-plataforma ">
             <p>Mi objetivo a largo plazo es un mundo más justo, donde los animales en zonas de pocos recursos puedan sobrevivir y ser adoptados, mientras personas que se lucran haciendo criar animales en paises desarrollados y estimulando el abandono sean limitados. Para ello, es necesaria una estructura firme y global, un desarrollo y mantenimiento continuo, y sobretodo personas... Por eso, te invito a colaborar como puedas! Si donas, o conoces a alguien que quiera aportar( desarrollo web, legal, marketing) podríamos cambiar el mundo juntos!</p>
             <iframe id='kofiframe' src='https://ko-fi.com/mariadevdesign/?hidefeed=true&widget=true&embed=true&preview=true' style='border:2px solid #0A526E;border-radius:20px ;width:100%;padding:4px;background:#f9f9f9;' height='570' title='mariadevdesign'></iframe>
           </div>
